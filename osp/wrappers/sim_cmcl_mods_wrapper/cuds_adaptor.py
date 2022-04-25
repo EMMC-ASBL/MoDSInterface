059d098fad810f076398a110998eb6429e9def53
dln22 <danieln@cmclinnovations.com>
2022-04-25 16:37:06 +0100

    extend_mods_input_schema: Added a comment on the simulation template varaible and used it for the CUDS to JSON translation.

--- osp/wrappers/sim_cmcl_mods_wrapper/cuds_adaptor.py
+++ osp/wrappers/sim_cmcl_mods_wrapper/cuds_adaptor.py
@@ -33,25 +33,34 @@ class CUDS_Adaptor:
 
         jsonData = defaultdict(list)
 
-        CUDS_Adaptor.algorithmsCUDStoJSON(
-            root_cuds_object=root_cuds_object,
-            jsonData = jsonData,
-        )
+        # atm only MOO simulation template is supported, so the check
+        # below, together with the template concept is not really needed.
+        # However, once things get complicated, e.g. more MoDS backend
+        # features will be supported, the template variable might be useful
+        # for picking predefined CUDStoJSON and JSONtoCUDS translation
+        # functions.
+        if simulation_template == engtempl.Engine_Template.MOO:
+            logger.info("Registering inputs")
 
-        CUDS_Adaptor.inputDataCUDStoJSON(
-            root_cuds_object=root_cuds_object,
-            jsonData = jsonData,
-        )
+            CUDS_Adaptor.algorithmsCUDStoJSON(
+                root_cuds_object=root_cuds_object,
+                jsonData = jsonData,
+            )
 
-        CUDS_Adaptor.inputAnalyticModelCUDStoJSON(
-            root_cuds_object=root_cuds_object,
-            jsonData = jsonData,
-        )
+            CUDS_Adaptor.inputDataCUDStoJSON(
+                root_cuds_object=root_cuds_object,
+                jsonData = jsonData,
+            )
 
-        CUDS_Adaptor.inputAnalyticModelCUDStoJSON(
-            root_cuds_object=root_cuds_object,
-            jsonData = jsonData,
-        )
+            CUDS_Adaptor.inputAnalyticModelCUDStoJSON(
+                root_cuds_object=root_cuds_object,
+                jsonData = jsonData,
+            )
+
+            CUDS_Adaptor.inputAnalyticModelCUDStoJSON(
+                root_cuds_object=root_cuds_object,
+                jsonData = jsonData,
+            )
 
         jsonDataStr = json.dumps(jsonData)
         return jsonDataStr
