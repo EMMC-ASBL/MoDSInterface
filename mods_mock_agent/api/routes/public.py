a439c880a0b9dec2878973aaa5148221f1ff27f6
dln22 <danieln@cmclinnovations.com>
2022-04-26 12:58:16 +0100

    extend_mods_input_schema: Fixed jobid key in the mods mock agent to match what the name  expected by the mods wrapper.

--- mods_mock_agent/api/routes/public.py
+++ mods_mock_agent/api/routes/public.py
@@ -33,7 +33,7 @@ def runSimulation():
     )
     jobId = str(uuid.uuid4())
     JOB_INPUTS[jobId] = outputs
-    return {"jobId": jobId}, 200
+    return {"jobID": jobId}, 200
 
 
 # Show an instructional message at the app root
@@ -41,7 +41,7 @@ def runSimulation():
 def getOutputs():
     # logger.info(request.args)
     query = json.loads(request.args["query"])
-    jobId = query["jobId"]
+    jobId = query["jobID"]
     outputs = {}
     try:
         outputs = JOB_INPUTS[jobId]
